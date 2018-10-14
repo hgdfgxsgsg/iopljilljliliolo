21b61e567a347b4b095dfe24eacaabd882d1176e
sruigtbuyktbukyh6y5d4y5h1dt841rd5th1sd <44135831+sruigtbuyktbukyh6y5d4y5h1dt841rd5th1sd@users.noreply.github.com>
2018-10-14 18:20:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function clean(text) {
         return text;
 }
 
-const prefix = "-";
+const prefix = "#";
 const token = "process.env.BOT_TOKEN";
 
 client.on("ready", () => {
@@ -38,10 +38,10 @@ client.on("message", (message) => {
 
 if (message.content.toLowerCase().startsWith(prefix + `بلاغ`)) {
     const reason = message.content.split(" ").slice(1).join(" ");
-    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+    if (!message.guild.roles.exists("name", "Support")) return message.channel.send(`لا يوجد رتبه ب اسم `مساعدة ` الرجاء من صاحب السيرفر انشاء رتبه بهاذا الاسم`.`);
+    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`يوجد لديك بلفعل بلاغ.`);
     message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
-        let role = message.guild.roles.find("name", "Support");
+        let role = message.guild.roles.find("name", "مساعدة");
         let role2 = message.guild.roles.find("name", "@everyone");
         c.overwritePermissions(role, {
             SEND_MESSAGES: true,
@@ -58,15 +58,15 @@ if (message.content.toLowerCase().startsWith(prefix + `بلاغ`)) {
         message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
         const embed = new Discord.RichEmbed()
         .setColor(0xCF40FA)
-        .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Team** will be here soon to help.`)
+        .addField(`Hey ${message.author.username}!`, `تم فتح التذكره بنجاح سيتم التواصل معك في اقرب وقت`)
         .setTimestamp();
         c.send({ embed: embed });
     }).catch(console.error);
 }
 if (message.content.toLowerCase().startsWith(prefix + `اغلاق`)) {
-    if (!message.channel.name.startsWith(`بلاغ-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+    if (!message.channel.name.startsWith(`بلاغ-`)) return message.channel.send(`لا يمكنك فعل هاذا الا في شات التذكره.`);
 
-    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-تاكيد\`. This will time out in 10 seconds and be cancelled.`)
+    message.channel.send(`هل انت متاكد من اقفال التذكره؟؟ اذا كنت متاكد اكتب / -تاكيد.`)
     .then((m) => {
       message.channel.awaitMessages(response => response.content === '-تاكيد', {
         max: 1,
